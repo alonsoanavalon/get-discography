b306fba53f99a2d1e1e950115a7269be4cb3517d
k3yzen <alonso.anavalon@gmail.com>
2022-05-08 18:33:17 -0400

    [Config] - [FEAT] - configuration file added

---
+++ src/config/configuration.ts
@@ -0,0 +1,22 @@
+export default () => ({
+  environment: process.env.ENVIRONMENT,
+  port: parseInt(process.env.PORT, 10),
+  headers: {
+    timezone: process.env.TIMEZONE,
+    environment: process.env.ENVIRONMENT,
+    countries: process.env.COUNTRIES,
+  },
+  pullLimit: process.env.GCLOUD_PUBSUB_SUBSCRIPTION_PULL_LIMIT,
+  api: {
+    topic: {
+      endpoint: process.env.API_TASKS_ENDPOINT,
+    },
+  },
+  pubsub: {
+    pullLimit: process.env.GCLOUD_PUBSUB_SUBSCRIPTION_PULL_LIMIT,
+    subscriptionName: process.env.GCLOUD_PUBSUB_SUBCRIPTION_NAME,
+    subscriberB64: process.env.GCLOUD_PUBSUB_SUBSCRIBER_B64,
+    projectId: process.env.GCLOUD_PROJECT_ID,
+    topicId: process.env.GCLOUD_PUBSUB_TOPIC_ID,
+  },
+});
